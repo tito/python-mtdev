867cdf430b57998b8017eaeac637c15725b2f46c
Mathieu Virbel <mathieu@pymt.eu>
2010-08-24 11:12:36 +0200

    use mtdev version for setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import mtdev
 
 setup(name='python-mtdev',
-      version='1.0',
+      version=mtdev.__version__,
       description='Python bindings for libmtdev',
       author='Mathieu Virbel',
       author_email='mathieu@pymt.eu',
